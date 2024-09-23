d9ca16ac6e44984a1a1d238b2342e72c73ce22b5
stupnikova <stupnikova_dv@ascon.ru>
2024-09-23 21:18:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Введите первое число");
+//преобразуем введенное значение из строки в вещественное
+double first_number = Convert.ToDouble(Console.ReadLine());
+first_number = Convert.ToDouble(first_number * 1.0);
+Console.WriteLine("Введите второе число");
+//преобразуем введенное значение из строки в вещественное
+double second_number = Convert.ToDouble(Console.ReadLine());
+second_number = Convert.ToDouble(second_number * 1.0);
+Console.WriteLine("Введите оператор");
+char action = Convert.ToChar(Console.ReadLine());
+double result;
+switch (action)
+{
+    //если введен оператор деления
+    //добавить обработку ошибок...
+    case '/':
+        result = first_number / second_number;
+        Console.WriteLine("Результат: " + result);
+        break;
+    //если введен оператор сложения
+    case '+':
+        result = first_number + second_number;
+        Console.WriteLine("Результат: " + result);
+        break;
+    //если введен оператор вычитания
+    case '-':
+        result = first_number - second_number;
+        Console.WriteLine("Результат: " + result);
+        break;
+    //если введен оператор умножения
+    case '*':
+        result = first_number * second_number;
+        Console.WriteLine("Результат: " + result);
+        break;
+    //если оператор не подошел
+    default:
+        Console.WriteLine("Введен неверный оператор!");
+        break;
+}
+Console.ReadKey();
